bed44ec5030662f8228e0b4e9c840feb34cd45da
Alexoid1 <alex_zam@hotmail.es>
2021-01-08 19:11:04 -0500

    add form message and routes

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// database mongo
+var mongoDB = 'mongodb+srv://';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -20,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/new', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+
+let messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Message Board' });
+  res.render('index', { title: 'Message Board',message:messages });
 });
 
 router.get('/new', function(req, res, next) {
+  console.log('jmghjmhg')
   res.render('new', { title: 'Express' });
 });
 
 router.post('/new', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  let newMessage = {
+    
+    text: req.body.message,
+    user: req.body.full_name,
+    added: new Date()
+  } 
+  messages.unshift(newMessage);
+  res.redirect('/');
 });
 
 module.exports = router;
